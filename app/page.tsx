c0f2b04eb4e55d4fb1fb3fffe5b055aabd7b1ad8
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2025-09-14 23:53:21 +0100

    work on slider and aboutus section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Nav from './components/Nav';
 import Hero from './components/Hero';
+import Aboutus from './components/Aboutus';
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Hero />
+      <Aboutus />
     </>
   );
 }
